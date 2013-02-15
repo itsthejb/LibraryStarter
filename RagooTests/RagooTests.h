86d94d2e3ae21d26cafdbd7d25a45f1043e60b5b
Jon Crooke <joncrooke@gmail.com>
2013-02-15 18:20:41 +0100

    Project setup

---
+++ RagooTests/RagooTests.h
@@ -0,0 +1,13 @@
+//
+//  RagooTests.h
+//  RagooTests
+//
+//  Created by Jon Crooke on 15/02/2013.
+//  Copyright (c) 2013 jc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RagooTests : SenTestCase
+
+@end
